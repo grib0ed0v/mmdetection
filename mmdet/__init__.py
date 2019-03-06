35c0f69bd83658a6fbb1b1aeeadaf59711814b61
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-03-06 14:41:03 +0300

    Manually specify version to allow running without install

--- mmdet/__init__.py
+++ mmdet/__init__.py
@@ -1,3 +1,4 @@
-from .version import __version__, short_version
-
+#from .version import __version__, short_version
+__version__ = '.6'
+short_version = '.6'
 __all__ = ['__version__', 'short_version']
