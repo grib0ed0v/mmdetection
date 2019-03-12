60d48fbf5f80444df8e5d12944b1f51f9a59808c
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-03-12 18:31:43 +0300

    Implement almost fair MobilenetV2-SSD

--- mmdet/models/backbones/mobilenetv2.py
+++ mmdet/models/backbones/mobilenetv2.py
@@ -8,23 +8,23 @@ from mmcv.cnn import (xavier_init, constant_init, kaiming_init,
 from mmcv.runner import load_checkpoint
 from ..registry import BACKBONES
 
-def conv_bn(inp, oup, stride):
+def conv_bn(inp, oup, stride, groups=1):
     return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False, groups=groups),
         nn.BatchNorm2d(oup),
         nn.ReLU6(inplace=True)
     )
 
 
-def conv_1x1_bn(inp, oup):
+def conv_1x1_bn(inp, oup, groups=1):
     return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False, groups=groups),
         nn.BatchNorm2d(oup),
         nn.ReLU6(inplace=True)
     )
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio):
+    def __init__(self, inp, oup, stride, expand_ratio, ssd_output=False):
         super(InvertedResidual, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
@@ -43,7 +43,7 @@ class InvertedResidual(nn.Module):
                 nn.BatchNorm2d(oup),
             )
         else:
-            self.conv = nn.Sequential(
+            self.conv = [
                 # pw
                 nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(hidden_dim),
@@ -55,13 +55,26 @@ class InvertedResidual(nn.Module):
                 # pw-linear
                 nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup),
-            )
+            ]
+        self.ssd_output = ssd_output
+        if self.ssd_output:
+            self.conv1 = nn.Sequential(*self.conv[0 : 3])
+            self.conv2 = nn.Sequential(*self.conv[3 :])
+        else:
+            self.conv = nn.Sequential(*self.conv)
+
+    def compute_conv(self, x):
+        if not self.ssd_output:
+            return self.conv(x)
+
+        expanded_x = self.conv1(x)
+        return (expanded_x, self.conv2(expanded_x))
 
     def forward(self, x):
         if self.use_res_connect:
-            return x + self.conv(x)
+            return x + self.compute_conv(x)
         else:
-            return self.conv(x)
+            return self.compute_conv(x)
 
 @BACKBONES.register_module
 class SSDMobilenetV2(nn.Module):
@@ -74,7 +87,7 @@ class SSDMobilenetV2(nn.Module):
         width_mult = 1.0
         block = InvertedResidual
         input_channel = 32
-        last_channel = 512
+        last_channel = 320
         interverted_residual_setting = [
             # t, c, n, s
             [1, 16, 1, 1],
@@ -87,7 +100,6 @@ class SSDMobilenetV2(nn.Module):
         ]
 
         # building first layer
-        self.out_feature_indices = []
         input_channel = int(input_channel * width_mult)
         self.last_channel = int(last_channel * width_mult) if width_mult > 1.0 else last_channel
         self.bn_first = nn.BatchNorm2d(3)
@@ -96,18 +108,34 @@ class SSDMobilenetV2(nn.Module):
         for t, c, n, s in interverted_residual_setting:
             output_channel = int(c * width_mult)
             for i in range(n):
+                ssd_output = False
+                if i == 0 and c == 160 and s == 2:
+                    ssd_output = True
+
                 if i == 0:
-                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                    self.features.append(block(input_channel, output_channel, s, t, ssd_output))
                 else:
-                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                    self.features.append(block(input_channel, output_channel, 1, t, ssd_output))
                 input_channel = output_channel
-                if s == 2 and i == n - 1:
-                    self.out_feature_indices.append(len(self.features) - 1)
-        # building last several layers
-        self.extra_conv = conv_1x1_bn(input_channel, self.last_channel)
-        #self.out_feature_indices.append(len(self.features) - 1)
         # make it nn.Sequential
         self.features = nn.Sequential(*self.features)
+
+        # building last several layers
+        self.extra_convs = []
+        self.extra_convs.append(conv_1x1_bn(last_channel, 1280))#head
+        self.extra_convs.append(conv_1x1_bn(1280, 256))
+        self.extra_convs.append(conv_bn(256, 256, 2, groups=256))
+        self.extra_convs.append(conv_1x1_bn(256, 512, groups=1))#head
+        self.extra_convs.append(conv_1x1_bn(512, 128))
+        self.extra_convs.append(conv_bn(128, 128, 2, groups=128))
+        self.extra_convs.append(conv_1x1_bn(128, 256))#head
+        self.extra_convs.append(conv_1x1_bn(256, 128))
+        self.extra_convs.append(conv_bn(128, 128, 2, groups=128))
+        self.extra_convs.append(conv_1x1_bn(128, 256))#head
+        self.extra_convs.append(conv_1x1_bn(256, 64))
+        self.extra_convs.append(conv_bn(64, 64, 2, groups=64))
+        self.extra_convs.append(conv_1x1_bn(64, 128))#head
+        self.extra_convs = nn.Sequential(*self.extra_convs)
 
     def init_weights(self, pretrained=None):
         if isinstance(pretrained, str):
@@ -131,9 +159,13 @@ class SSDMobilenetV2(nn.Module):
         x = self.bn_first(x)
         for i, block in enumerate(self.features):
             x = block(x)
-            if i in self.out_feature_indices:
+            if isinstance(x, tuple):
+                outs.append(x[0])
+                x = x[1]
+
+        for i, conv in enumerate(self.extra_convs):
+            x = conv(x)
+            if i % 3 == 0:
                 outs.append(x)
-        x = self.extra_conv(x)
-        outs.append(x)
 
         return tuple(outs)
